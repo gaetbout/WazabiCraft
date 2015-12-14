cd9d4535a815dd4ee985de1f0f0357ce61aa9fc1
gaetbout <gaetan.ansotte@student.vinci.be>
2015-12-14 17:04:19 +0100

    WEB UI
    Ajout

---
+++ WebUI/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebUI.ServiceReferenceJoueur;
+
+namespace WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IGestionJoueur Repository;
+
+        public HomeController(IGestionJoueur personneRepository)
+        {
+            this.Repository = personneRepository;
+        }
+
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult Resume()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Connexion()
+        {
+            return View(new JoueurClient());
+        }
+
+        [HttpPost]
+        public ActionResult Connexion(JoueurClient login)
+        {
+            ModelState.Remove("ConfirmPassword");
+
+            if (ModelState.IsValid)
+            {
+                JoueurClient personne = Repository.Connexion(login);
+                if (personne == null)
+                {
+                    ModelState.AddModelError("Fail", "Vos données sont incorrectes");
+                    return View(login);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Principal");
+                }
+            }
+            else
+            {
+                return View(login);
+            }
+        }
+
+    }
+}
---
+++ WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,40 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Ninject;
+using WebUI.ServiceReferenceJoueur;
+using WebUI.ServiceReferencePartie;
+
+#endregion
+
+namespace WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private readonly IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            kernel.Bind<IGestionJoueur>().To<GestionJoueurClient>();
+            kernel.Bind<IGestionPartie>().To<GestionPartieClient>();
+        }
+    }
+}
